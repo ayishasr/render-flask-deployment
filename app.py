2200f5c93e07368c232d5ffd89efb481e76c457d
Ayisha Saheed <ayishasr6@gmail.com>
2025-04-04 22:56:35 +0530

    Solved the error method not allowed

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-# Flask (app.py)
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Welcome to the Flask Server</h1>"
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    return jsonify({'prediction': 'This is the prediction'})
 
 if __name__ == '__main__':
     app.run()
